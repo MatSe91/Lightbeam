74a32dc60387d904125678b6c1cb0caaad5734f4
matse91 <matthiasseyfarth@web.de>
2016-03-07 17:46:35 +0100

    Testscripte Collectable + Wassertropfen Sprite

---
+++ programm/Testumgebung/Assets/Scripts/MirrorColorReflection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirrorColorReflection : MonoBehaviour {
+
+    public Farbe lichtFarbe = Farbe.Weiss;
+
+    public enum Farbe{     Weiss,
+                    Rot,
+                    Orange,
+                    Gelb,
+                    Gruen,
+                    Hellblau,
+                    Indigo,
+                    Violett,
+                    All
+    }
+
+    public Farbe getFarbe()
+    {
+        return this.lichtFarbe;
+    }
+    
+}

e258698c5aca5af69f3e967b6495eba545826b85
Rojey <roman.jung.rj89@gmail.com>
2016-03-16 11:18:44 +0100

    Collider funktionieren nun richtig und werden entsprechend der Fastlinepropertys angelegt

--- programm/Testumgebung/Assets/Scripts/BeamCollider.cs
+++ programm/Testumgebung/Assets/Scripts/BeamCollider.cs
@@ -1,24 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
 
 namespace DigitalRuby.FastLineRenderer
 {
-    public class BeamCollider : MonoBehaviour
+    public  class BeamCollider : MonoBehaviour
     {
-
+       
         // Use this for initialization
 
-        private static BoxCollider box;
-
-
-
-        public static void AddColliderToLine(Vector3 startPos, Vector3 endPos, FastLineRenderer r)
+        public static void     AddColliderToLine(Vector3 startPos, Vector3 endPos, FastLineRenderer r)//,GameObject Prefab)
         {
+
+            // GameObject Beamcollider = Instantiate(Prefab);
+            // GameObject Beamcollider = new GameObject("Beamcollider");
             BoxCollider box = new GameObject("Collider").AddComponent<BoxCollider>();
-            box.transform.parent = r.transform; // Collider is added as child object of line
+            box.transform.parent = r.transform;
+            //BoxCollider box = Beamcollider.AddComponent<BoxCollider>();
+            // BoxCollider box = Beamcollider.GetComponent<BoxCollider>();
+            //Vector3 midPoint = (startPos + endPos) / 2;
+            // box.transform.position = midPoint; // Collider is added as child object of line
             float lineLength = Vector3.Distance(startPos, endPos); // length of line
-            box.size = new Vector3(lineLength, 0.1f, 1f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
-            box.center = new Vector3(lineLength / 2, 0.0f, 0f);
+            
+            
+           box.size = new Vector3(lineLength , 0.1f, 1f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
+           // box.center = new Vector3(box.size.x / 2, 0.0f, 0f);
+           
             Vector3 midPoint = (startPos + endPos) / 2;
             box.transform.position = midPoint; // setting position of collider object
                                                // Following lines calculate the angle between startPos and endPos
@@ -28,14 +36,29 @@ namespace DigitalRuby.FastLineRenderer
                 angle *= -1;
             }
             angle = Mathf.Rad2Deg * Mathf.Atan(angle);
-            box.transform.Rotate(0, 0, angle);
-            //box.tag = "BeamCollider";
-            box.enabled = true;
-            //Debug.Log(box.size);
-            //Debug.Log(box.center);
-            //Debug.Log(box.tag);
 
+            
+            box.transform.eulerAngles = new Vector3(0, 0, angle);
+            // box.tag = "BeamCollider";
+            box.gameObject.layer = 9;
+            box.enabled = true;
+            //Debug.Log("size" + box.size);
+            //Debug.Log("center" + box.center);
+            //Debug.Log("position" + box.transform.position);
+            //Debug.Log("rotation" + box.transform.rotation);
+            //Debug.Log("angle" + angle);
+            
+            //Debug.Log(box.tag);
+            //return Beamcollider;
 
         }
+
+        //public  static void DeleteCollider(IEnumerable  collider)
+        //{
+        //    foreach (GameObject coll in collider)
+        //    {
+        //        Destroy(coll);
+        //    }
+        //}
     }
 }
---
+++ programm/Testumgebung/Assets/Scripts/BeamScript_RJ.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace DigitalRuby.FastLineRenderer
+{
+    public class BeamScript_RJ : MonoBehaviour
+    {
+        [Tooltip("Tag an dem der Spiegel reflektiert.")]
+        public string MirrorTag;
+       
+        [Tooltip("Hier wird der Layer des Collectable ausgewählt")]
+        public LayerMask checkPointLayer;
+
+        private FastLineRenderer r;
+        private FastLineRendererProperties property;
+        private BeamRefraction br;
+        private List<FastLineRendererProperties> properties;
+
+        private GameObject sameObject;
+        private bool isActive;
+        private Vector3 curPosition;
+        private Vector3 dir;
+        private bool beamIsInWater = false;
+        Color intensitive;
+      
+
+        // Use this for initialization
+        void Start()
+        {
+            r = FastLineRenderer.CreateWithParent(null, GetComponent<FastLineRenderer>());
+            property = new FastLineRendererProperties();
+          ;
+           
+            intensitive = new Color(0, 0, 0, 0.7f);
+            br = new BeamRefraction();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (r != null)
+            {
+                r.Reset();
+                properties = new List<FastLineRendererProperties>();
+                
+            }
+
+
+
+            isActive = true;
+            curPosition = transform.position;
+            property.Start = curPosition;
+            standardColor();
+            dir = transform.right;
+
+            while (isActive)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(curPosition, dir, out hit))
+                {
+                    if (hit.transform.gameObject.tag == MirrorTag)
+                    {
+                        isActive = true;
+                        curPosition = hit.point;
+                        dir = Vector3.Reflect(dir, hit.normal);
+                        property.End = curPosition;
+                        //BeamCollider.AddColliderToLine(property.Start, property.End, r);
+                        properties.Add(property);
+                        property = new FastLineRendererProperties();
+                        standardColor();
+                        // r.AddLine(property);
+                        property.Color = Color.green;
+                        if (beamIsInWater)
+                        {
+                            reduceBeamIntencity();
+                        }
+                        property.Start = curPosition;
+                    }
+                    else
+                    {
+                        isActive = false;
+                    }
+
+                    if (hit.transform.gameObject.layer == 4)
+                    {
+                        //Beam befindet sich noch nicht im Wasser
+                        if (!beamIsInWater)
+                        {
+
+                            dir = br.refraction_Angle(Vector3.Angle(dir, hit.normal), hit, Refraction_Medium.air, Refraction_Medium.water);
+
+                            curPosition = hit.point;
+                            property.End = curPosition;
+                            //BeamCollider.AddColliderToLine(property.Start, property.End, r);
+                            properties.Add(property);
+                            property = new FastLineRendererProperties();
+                            standardColor();
+                            // r.AddLine(property);
+                            property.Color = Color.blue;
+
+                            //property.Color = property.Color - intensitive;
+                            property.Start = curPosition;
+
+                            beamIsInWater = true;
+
+                        }
+                        else    // Beam befindet sich bereits im Wasser
+                        {
+                            beamIsInWater = false;
+                        }
+
+                        isActive = true;
+                    }
+
+                    // Prüfe ob die angegebene Maske mit der Maske im hit übereinstimmt
+                    if ((checkPointLayer.value & 1 << hit.transform.gameObject.layer) == 1 << hit.transform.gameObject.layer)
+                    {
+                        BeamConnectivity(hit.transform.gameObject, true);
+                        sameObject = hit.transform.gameObject;
+                    }
+
+                    property.End = hit.point;
+
+                }
+                else
+                {
+                    isActive = false;
+                    property.End = curPosition + dir * 20;
+                    //properties.Add(property);
+
+                    if (sameObject != null)
+                    {
+                        BeamConnectivity(sameObject, false);
+                    }
+                }
+            }
+            addLines();
+        }
+
+        private void BeamConnectivity(GameObject checkP, bool bo)
+        {
+            checkP.GetComponent<CheckPointManager>().setBeamConnectivity(bo);
+        }
+
+
+        private void standardColor()
+        {
+            property.Radius = 0.1f;
+            property.Color = Color.red;
+        }
+
+       
+
+        private void reduceBeamIntencity()
+        {
+            property.Color = property.Color - intensitive;
+        }
+
+        private void addLines()
+        {
+            properties.Add(property);
+
+            foreach (var prop in properties)
+            {
+                r.AddLine(prop);
+               
+                 BeamCollider.AddColliderToLine(prop.Start,prop.End,r);
+               
+            }
+            r.Apply(true);
+
+        }
+
+      
+
+
+
+    }
+
+}
+
+
+

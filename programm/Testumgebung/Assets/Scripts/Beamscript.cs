9d8825fad4bd2aaa18ad86a1e54c12e51f308137
matse91 <matthiasseyfarth@web.de>
2016-03-25 14:46:17 +0100

    Update call wird nur durchgeführt wenn etwas getouched wird, Costumcleanup() erzeugt

--- programm/Testumgebung/Assets/Scripts/Beamscript.cs
+++ programm/Testumgebung/Assets/Scripts/Beamscript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DigitalRuby.FastLineRenderer
 {
@@ -30,106 +31,113 @@ namespace DigitalRuby.FastLineRenderer
         void Start()
         {
             r = FastLineRenderer.CreateWithParent(null, GetComponent<FastLineRenderer>());
+            properties = new List<FastLineRendererProperties>();
             intensitive = new Color(0, 0, 0, 0.7f);
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (r != null)
+            if (InputManager.touchInput)
             {
-                r.Reset();
-                properties = new List<FastLineRendererProperties>();
-                property = new FastLineRendererProperties();
-                property.LineInWater = false;
-                beamIsInWater = false;
-            }
-
-            isActive = true;
-            curPosition = transform.position;
-            property.Start = curPosition;
-            standardColor();
-            dir = transform.right;
-
-            while (isActive)
-            {
-                RaycastHit hit;
-                if (Physics.Raycast(curPosition, dir, out hit))
+                if (r != null)
                 {
-                    if (hit.transform.gameObject.tag == MirrorTag)
-                    {
-                        isActive = true;
-                        curPosition = hit.point;
-                        dir = Vector3.Reflect(dir, hit.normal);
-                        property.End = curPosition;
+                    costumCleanup(r.meshes);
+                    r.Reset();
+                    BeamCollider.OnDestroy();
+                    properties.Clear();
+                    property = new FastLineRendererProperties();
+                    property.LineInWater = false;
+                    beamIsInWater = false;
+                }
 
-                        properties.Add(property);
-                        property = new FastLineRendererProperties();
-                        standardColor();
-                        property.Color = Color.green;
-                        property.Start = curPosition;
+                isActive = true;
+                curPosition = transform.position;
+                property.Start = curPosition;
+                standardColor();
+                dir = transform.right;
+
+                while (isActive)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(curPosition, dir, out hit))
+                    {
+                        if (hit.transform.gameObject.tag == MirrorTag)
+                        {
+                            isActive = true;
+                            curPosition = hit.point;
+                            dir = Vector3.Reflect(dir, hit.normal);
+                            property.End = curPosition;
+
+                            properties.Add(property);
+                            property = new FastLineRendererProperties();
+                            standardColor();
+                            property.Color = Color.green;
+                            property.Start = curPosition;
+                        }
+                        else
+                        {
+                            isActive = false;
+                        }
+
+                        if (hit.transform.gameObject.layer == 4)
+                        {
+                            curPosition = hit.point;
+                            property.End = curPosition;
+                            properties.Add(property);
+                            property = new FastLineRendererProperties();
+                            standardColor();
+
+                            if (!beamIsInWater)
+                            {
+                                br = new BeamRefraction(dir, hit, Refraction_Medium.Refraction_Med.air, Refraction_Medium.Refraction_Med.water);
+                                dir = br.getDir();
+                                beamIsInWater = br.getLineInWater();
+                            }
+                            else
+                            {
+                                br = new BeamRefraction(dir, hit, Refraction_Medium.Refraction_Med.water, Refraction_Medium.Refraction_Med.air);
+                                dir = br.getDir();
+                                beamIsInWater = br.getLineInWater();
+                            }
+                            property.Color = Color.blue;
+                            property.Start = curPosition;
+                            isActive = true;
+                        }
+
+                        // Prüfe ob die angegebene Maske mit der Maske im hit übereinstimmt
+                        if ((checkPointLayer.value & 1 << hit.transform.gameObject.layer) == 1 << hit.transform.gameObject.layer)
+                        {
+                            BeamConnectivity(hit.transform.gameObject, true);
+                            sameObject = hit.transform.gameObject;
+                        }
+
+                        if (beamIsInWater)
+                        {
+                            property.LineInWater = true;
+                        }
+                        else
+                        {
+                            property.LineInWater = false;
+                        }
+                        property.End = hit.point;
+
                     }
                     else
                     {
                         isActive = false;
-                    }
+                        property.End = curPosition + dir * 20;
 
-                    if (hit.transform.gameObject.layer == 4)
-                    {
-                        curPosition = hit.point;
-                        property.End = curPosition;
-                        properties.Add(property);
-                        property = new FastLineRendererProperties();
-                        standardColor();
-
-                        if (!beamIsInWater)
+                        if (sameObject != null)
                         {
-                            br = new BeamRefraction(dir, hit, Refraction_Medium.Refraction_Med.air, Refraction_Medium.Refraction_Med.water);
-                            dir = br.getDir();
-                            beamIsInWater = br.getLineInWater();
+                            BeamConnectivity(sameObject, false);
                         }
-                        else
-                        {
-                            br = new BeamRefraction(dir, hit, Refraction_Medium.Refraction_Med.water, Refraction_Medium.Refraction_Med.air);
-                            dir = br.getDir();
-                            beamIsInWater = br.getLineInWater();
-                        }
-                        property.Color = Color.blue;
-                        property.Start = curPosition;
-                        isActive = true;
-                    }
-
-                    // Prüfe ob die angegebene Maske mit der Maske im hit übereinstimmt
-                    if ((checkPointLayer.value & 1 << hit.transform.gameObject.layer) == 1 << hit.transform.gameObject.layer)
-                    {
-                        BeamConnectivity(hit.transform.gameObject, true);
-                        sameObject = hit.transform.gameObject;
-                    }
-
-                    if (beamIsInWater)
-                    {
-                        property.LineInWater = true;
-                    }
-                    else
-                    {
-                        property.LineInWater = false;
-                    }
-                    property.End = hit.point;
-
-                }
-                else
-                {
-                    isActive = false;
-                    property.End = curPosition + dir * 20;
-
-                    if (sameObject != null)
-                    {
-                        BeamConnectivity(sameObject, false);
                     }
                 }
+                properties.Add(property);
+                addLines();
+
             }
-            properties.Add(property);
-            addLines();
         }
 
         private void BeamConnectivity(GameObject checkP, bool bo)
@@ -159,6 +167,15 @@ namespace DigitalRuby.FastLineRenderer
             }
             r.Apply(true);
         }
+        private void costumCleanup(List<Mesh> meshes)
+        {
+            foreach (Mesh mesh in meshes)
+            {
+                if (mesh != null)
+                {
+                    Destroy(mesh);
+                }
+            }
+        }
     }
-
 }

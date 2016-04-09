5104dbbf87e5b8b4b8b35227e0e6d31d0c953bf2
matse91 <matthiasseyfarth@web.de>
2016-04-09 18:50:28 +0200

    .

--- programm/Testumgebung/Assets/Scripts/DoorOpener.cs
+++ programm/Testumgebung/Assets/Scripts/DoorOpener.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System;
 using System.Collections;
 
 public class DoorOpener: MonoBehaviour
@@ -11,73 +9,25 @@ public class DoorOpener: MonoBehaviour
     public bool WithColor;
     public int counter;
     public bool ConstantTrigger;
-    public bool doorIsOpen =false;
+    public bool dooIsOpen =false;
     private Collider other;
-
-    public int openDoorInSec; // wird später abgeknöpft von counter
-    public GameObject loadingDoor;
-    public GameObject activadedDoor;
-
-
 
     public void OpenDoor(CustomColor.CustomizedColor collisionColor)
     {
 
-        if (!WithColor && !doorIsOpen)
+        if (!WithColor && !dooIsOpen)
         {
            
             Debug.Log("Animation Door open");
-            doorIsOpen = true;
+            dooIsOpen = true;
         }
 
-        if (collisionColor == validColor && WithColor && !doorIsOpen)
+        if (collisionColor == validColor && WithColor && !dooIsOpen)
         {
            
             Debug.Log("Animation Door open");
-            doorIsOpen = true;
+            dooIsOpen = true;
         }
  
     }
-
-    private void activateLoadingDoor()
-    {
-        ParticleSystem parSys = loadingDoor.GetComponent<ParticleSystem>();
-        for (int i =0; i< openDoorInSec; i++)
-        {
-            parSys.Simulate(i);
-            StartCoroutine(moveOn(parSys));
-        }
-    }
-
-    private IEnumerator moveOn(ParticleSystem parSys)
-    {
-        if (!parSys.IsAlive())
-        {
-            
-        }
-        throw new NotImplementedException();
-    }
-
-
-    private void invokeLoadingDoor()
-    {
-
-    }
-
-    private void activateActiveDoorAnim()
-    {
-
-    }
-
-
-    // starte die Partikel mit X sekunden dauer
-    // Wenn lichtstrahl runter --> breche Partikel abspielen ab (aktuellen Loop beenden, nicht nur abbrechen)
-    // wenn Partikel dauer in Sec erfolgreich durchgelaufen
-        // beende Partikel
-        // öffne Tür
-        // spiele animation ab
-    
-    // ggf wenn licht von doorknop
-        // dann beende animation
-    // ggf selbe prozedure von vorn
 }

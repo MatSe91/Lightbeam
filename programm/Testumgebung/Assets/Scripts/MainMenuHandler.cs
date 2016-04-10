7fb3ff0840563dae8fe6198384c4e45d89fb7668
rojey89 <roman.jung.rj89@gmail.com>
2016-04-10 16:36:06 +0200

    MainMenuScene mit Assets und Einstellungen erstellt

---
+++ programm/Testumgebung/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public GameObject MainMenu;
+    private GameObject activeMenu;
+
+    void Start()
+    {
+        activeMenu = MainMenu;
+    }
+
+    public void ClickMusicButtton(Button musicButton)
+    {
+        if (!Settings.MusicVolume)
+        {
+            AudioListener.volume = 1f;
+            Settings.MusicVolume = true;
+            musicButton.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Images/music");
+        }
+        else
+        {
+            AudioListener.volume = 0f;
+            Settings.MusicVolume = false;
+            musicButton.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Images/music_off");
+        }
+    }
+
+    public void ClickSfxButtton(Button sfxButton)
+    {
+        if(Settings.SfxVolume)
+        { 
+            Settings.SfxVolume = false;
+            sfxButton.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Images/sfx_off");
+        }
+ 
+        else
+        {
+            Settings.SfxVolume = true;
+            sfxButton.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Images/sfx");
+        }
+    }
+
+    public void ClickNextMenu(GameObject nextMenu)
+    {
+        activeMenu.SetActive(false);
+        nextMenu.SetActive(true);
+        activeMenu = nextMenu;
+    }
+
+}

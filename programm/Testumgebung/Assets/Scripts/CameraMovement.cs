8aeec036d4f22f81d2c411f7f6a52eb3c61300a0
Rojey <roman.jung.rj89@gmail.com>
2016-04-14 18:24:06 +0200

    Cameramovement added Music added

---
+++ programm/Testumgebung/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float sensX = 100.0f;
+
+    void Update()
+    {
+
+        
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 tmp = transform.position + new Vector3(Input.GetAxis("Mouse X") * sensX * Time.deltaTime, 0, 0);
+
+            if (tmp.x >=minX && tmp.x <=maxX)
+            {
+                transform.position += new Vector3(Input.GetAxis("Mouse X") * sensX * Time.deltaTime, 0, 0);
+            }
+        }
+
+    }
+
+
+}

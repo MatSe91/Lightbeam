ded9584b3f840fc5e86b39502126303ede942fae
Rojey <roman.jung.rj89@gmail.com>
2016-05-12 17:59:23 +0200

    ChangebeamColor ergänzt

--- programm/Testumgebung/Assets/Scripts/ChangeBeamColor.cs
+++ programm/Testumgebung/Assets/Scripts/ChangeBeamColor.cs
@@ -6,9 +6,35 @@ using System.Text.RegularExpressions;
 public class ChangeBeamColor : MonoBehaviour {
 
     public CustomColor.CustomizedColor newColor;
+    private bool isChanged;
+
+    public bool IsChanged
+    {
+        get
+        {
+            return isChanged;
+        }
+
+        set
+        {
+            isChanged = value;
+        }
+    }
 
     public CustomColor.CustomizedColor getNewBeamColor()
     {
         return newColor;
+    }
+
+    public ChangeBeamColor Reflect(CustomColor.CustomizedColor collisionColor)
+    {
+       
+        IsChanged = false;
+        if (collisionColor == newColor)
+        {
+            isChanged = true;
+        }
+
+        return this;
     }
 }
